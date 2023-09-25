e043f4af7f2ca571d3cc9d83a672f77aacc16758
Mikhail Luzgin <luzginme84@gmail.com>
2023-09-26 01:47:31 +0300

    ref init

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,4 +1,6 @@
-from gendiff.data_output import generate_diff
+from gendiff.data_output import generate_diff, get_format_gen_diff
+from gendiff.parser import arg_parser
+from gendiff.work_fold.open_files import get_dict_from_files
 
 
-__all__ = [generate_diff]
+__all__ = [generate_diff, get_format_gen_diff, arg_parser, get_dict_from_files]
